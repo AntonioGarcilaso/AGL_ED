1095e04048cc2201562503c55618505bb339ff85
AntonioGarcilaso <antonio2004.garcia.lopez@gmail.com>
2025-06-03 19:12:29 +0200

    AGL_Coche.java

--- src/coches/AGL_Coche.java
+++ src/coches/AGL_Coche.java
@@ -1,11 +1,68 @@
 package coches;
 
-public class AGL_Coche {
+public class AGL_Moto {
 	private String Matricula;
 	private String Modelo;
- 	private int NumeroPlazas;
- 	private double VelocidadMedia;
- 	private String TipoCombustible;
- 	private double Consumo;
-	
+	private int NumeroPlazas;
+	private double VelocidadMedia;
+	private String TipoCombustible;
+	private double Consumo;
+	public AGL_Moto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public AGL_Moto(String matricula, String modelo, int numeroPlazas, double velocidadMedia, String tipoCombustible,
+			double consumo) {
+		super();
+		Matricula = matricula;
+		Modelo = modelo;
+		NumeroPlazas = numeroPlazas;
+		VelocidadMedia = velocidadMedia;
+		TipoCombustible = tipoCombustible;
+		Consumo = consumo;
+	}
+	public String getMatricula() {
+		return Matricula;
+	}
+	public void setMatricula(String matricula) {
+		Matricula = matricula;
+	}
+	public String getModelo() {
+		return Modelo;
+	}
+	public void setModelo(String modelo) {
+		Modelo = modelo;
+	}
+	public int getNumeroPlazas() {
+		return NumeroPlazas;
+	}
+	public void setNumeroPlazas(int numeroPlazas) {
+		NumeroPlazas = numeroPlazas;
+	}
+	public double getVelocidadMedia() {
+		return VelocidadMedia;
+	}
+	public void setVelocidadMedia(double velocidadMedia) {
+		VelocidadMedia = velocidadMedia;
+	}
+	public String getTipoCombustible() {
+		return TipoCombustible;
+	}
+	public void setTipoCombustible(String tipoCombustible) {
+		TipoCombustible = tipoCombustible;
+	}
+	public double getConsumo() {
+		return Consumo;
+	}
+	public void setConsumo(double consumo) {
+		Consumo = consumo;
+	}
+	@Override
+	public String toString() {
+		return "AGL_Moto [Matricula=" + Matricula + ", Modelo=" + Modelo + ", NumeroPlazas=" + NumeroPlazas
+				+ ", VelocidadMedia=" + VelocidadMedia + ", TipoCombustible=" + TipoCombustible + ", Consumo=" + Consumo
+				+ "]";
+	}
+
+
 }
