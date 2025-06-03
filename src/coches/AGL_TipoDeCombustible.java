dca2b54ed7ff7eb9013c3aa82791de4a668275e9
García López <antonio.garcia21@isepceu.es>
2025-06-03 19:30:00 +0200

    AGL_Commit de la segunda clase Moto y combustibles

---
+++ src/coches/AGL_TipoDeCombustible.java
@@ -0,0 +1,17 @@
+package coches;
+
+public enum AGL_TipoDeCombustible {
+		GASOLINA(1.60),
+		DIESEL(1.45),
+		ELECTRICO(0.20);
+		
+		private final double precioPorLitro;
+	
+		
+		AGL_TipoDeCombustible(double precioPorLitro) {
+			this.precioPorLitro = precioPorLitro;
+		}
+			public double getPrecioPorLitro() {
+				return precioPorLitro;
+			}
+		}
