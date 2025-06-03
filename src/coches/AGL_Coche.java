e61670ffa282ec43df110ff86cfc4ddeeb2d4dd2
García López <antonio.garcia21@isepceu.es>
2025-06-03 18:50:09 +0200

    AGL_Commit de la primera clase coche

---
+++ src/coches/AGL_Coche.java
@@ -0,0 +1,10 @@
+package coches;
+
+public class AGL_Coche {
+	private String Matricula;
+	private String Modelo;
+ 	private int NumeroPlazas;
+ 	private double VelocidadMedia;
+ 	private String TipoCombustible;
+ 	private double Consumo;
+}
